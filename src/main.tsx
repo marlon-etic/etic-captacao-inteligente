fa251696e96cf1a465ce6c4d92729f62f423b897
Skip <skip@usecurling.com>
2026-03-24 21:08:02 +0000

    Restore protected files

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 /* Main entry point for the application - renders the root React component */
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
 
 // @skip-protected: Do not remove. Required for React rendering.
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(<App />)
