48785dfe7c52fa24b2894a56e19ad50b6beb40c6
Skip <skip@usecurling.com>
2026-03-26 18:29:28 +0000

    Fix invalid credentials error in login

---
+++ supabase/migrations/20260326184500_fix_marlon_password.sql
@@ -0,0 +1,7 @@
+DO $body$
+BEGIN
+  -- Update password for both emails to 'kissarmy0440' to match the user's latest attempt
+  UPDATE auth.users 
+  SET encrypted_password = crypt('kissarmy0440', gen_salt('bf'))
+  WHERE email IN ('marlonjmoro@hotmail.com', 'marlon@eticimoveis.com.br');
+END $body$;
