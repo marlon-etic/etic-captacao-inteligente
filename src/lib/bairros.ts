6f51a85fbbce09aa711a533ab003ec5469554abc
Skip <skip@usecurling.com>
2026-03-30 10:54:58 +0000

    Auto-commit before sync

--- src/lib/bairros.ts
+++ src/lib/bairros.ts
@@ -1,5 +1,4 @@
 import { REGIONS_DATA } from './regions'
 
 export const BAIRROS_ETIC = Array.from(
-  new Set(REGIONS_DATA.flatMap((r) => [r.anchor, ...r.satellites])),
-).sort()
+  new Set(REGIONS_DATA.flatMap((r) => [r.anchor, ...
